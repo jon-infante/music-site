c67f4a0ae79db0a3d07a3aa2beb0149c22248624
inv0w <jonjoninfante@gmail.com>
2020-04-21 15:52:14 -0700

    Updating models

--- music/models.py
+++ music/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Musician(models.Model):
     name = models.CharField(max_length=50)
+    age = models.IntegerField(null=True)
 
     def __str__(self):
         return self.name
